0a73b409ce18784379cb7be6b3160463bf021373
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2025-09-10 23:12:52 +0300

    Course n.51

---
+++ ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{   
+    public class HomeController 
+    {
+        [Route("sayhello")]
+        public string method1()
+        {
+            return "Hello World";
+        }
+    
+    }
+}
--- ControllersExample/Program.cs
+++ ControllersExample/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();//Adds all Controllers
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();//Call Controllers
 app.Run();
